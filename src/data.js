5c1cf259724e4d722ae4e3e3729e479f0e4deb79
Prashant Bhardwaj <geekyprashant840@gmail.com>
2023-04-06 12:25:17 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    name: "Prashant Bhardwaj",
+    age: 22,
+    image: "/images/prashant.png",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+];
